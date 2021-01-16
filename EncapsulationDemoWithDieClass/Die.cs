adf3ea5e46138f9bb793e5d927dd98fffc4d7b2b
dunn0056@icloud.com <Sakura!13531>
2021-01-16 05:04:19 -0800

    Refactored code and added min & max value for die

--- EncapsulationDemoWithDieClass/Die.cs
+++ EncapsulationDemoWithDieClass/Die.cs
@@ -8,15 +8,32 @@ namespace EncapsulationDemoWithDieClass
     {
         // static fields are shared across all instances of a class
         static Random rand;
+        byte minValue;
+        byte maxValue;
 
         static Die() // Static constructors are called once for all instances of this class
         {
             rand = new Random();
         }
 
-        public Die()
+        public Die():this(1, 6)
         {
-            Roll(); // Roll die on creation to generate first random number
+            // minValue = 1;
+            // maxValue = 6;
+            // Roll(); // Roll die on creation to generate first random number
+        }
+
+        /// <summary>
+        /// Creates a die with numbers between the minimum
+        /// and maximum values
+        /// </summary>
+        /// <param name="minValue">The inclusive lower bound</param>
+        /// <param name="maxValue">The inclusive maximum bound</param>
+        public Die(byte minValue, byte maxValue)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            Roll();
         }
 
         /// <summary>
@@ -27,7 +44,7 @@ namespace EncapsulationDemoWithDieClass
         public bool IsHeld { get; set; }
 
         /// <summary>
-        /// Roll a new random value between 1 - 6
+        /// Roll a new random value between minimum and maximum value
         /// and return the newly rolled value
         /// If the die is held, the current value will be returned
         /// and no new value generated
@@ -36,7 +53,8 @@ namespace EncapsulationDemoWithDieClass
         {
             if (!IsHeld) // If die is not held
             {
-                Value = (byte)rand.Next(1, 7);
+                // Add 1 to maxValue due to the upper bound being exclusive
+                Value = (byte)rand.Next(minValue, maxValue + 1);
             }
             return Value;
         }
